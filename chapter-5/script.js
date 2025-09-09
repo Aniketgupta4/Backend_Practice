d019a4402db110dc7008213a2c6a8f17db3b9286
Aniketgupta4 <aniketkumargupta302@gmail.com>
2025-09-09 23:50:52 +0530

    chapter-5

---
+++ chapter-5/script.js
@@ -0,0 +1,55 @@
+//     op session
+//
+//     ex: 1) uber -> new work hota rehta hai -> so new people ki need hoti hai
+//
+//                /////////uber/////////
+//                 1) discount team             
+//                 2) marketing team                                   <- ** in sabke pass puran data hota hai -> so ye sab pattern find karte hai data se and new mai -> is type ka same hua toh pta chal jayega -> fraud hora hai
+//                 3) customer support team
+//                 4) fraud handler team -> handle fraud in app or override toh ni karra hai
+//                 5) data science team
+//                 6) bill desk team -> online payment hua hai toh usko handle karna hai
+//                 
+// 
+// 
+//             -> ** basically bohot sara data arise hota hai -> usko handle karna padta hai -> so handle karne ke liye -> team banate hai -> ye work hota hai
+// 
+//             -> in uber new work aate rehte hai -> ex add chalne lagta hai -> so usme fraud hone ke chances badh jate hai -> so usko handle karne ke liye team banate hai
+// 
+// 
+//
+//
+//            -> 2) ** sdlc -> software development life cycle -> requirements -> design -> implementation -> testing -> deployment -> maintenance
+//                       
+//                       -> requirement -> product/product manager -> business analyst -> ye log pta karte hai ki kya chahiye -> ui + language + framework + paid user chahiye ya free user chahiye -> ye sab pta karte hai -> srs banega -> software requirement specification
+//                       -> design -> senior developer -> sd4 , sd5 high level ke -> ye log design karte hai ki kya chahiye -> architecture banate hai -> low level design + high level design -> database design -> frontend , backend -> ye sab karte hai 
+//                       -> implementation -> coding -> junior developer -> sd1, sd2, sd3 low level ke -> ye log coding karte hai
+//                       -> testing -> testing team -> ye log testing karte hai -> manual testing + automation testing -> ye sab karte hai
+//                       -> deployment -> devops engineer -> ye log deployment karte hai -> aws , azure , gcp -> ye sab karte hai
+//                       -> maintenance -> maintenance team -> ye log maintenance karte hai -> bug fix , update , upgrade -> ye sab karte hai
+// 
+//                       -> ** ye sab team alag alag hoti hai -> har team ka apna apna kaam hota hai -> so ye sab milke ek software banate hai
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+//
+//
