1a0c0d36b7a880c17514925e2b03af645c901582
Aniketgupta4 <aniketkumargupta302@gmail.com>
2025-09-10 22:52:55 +0530

    chapter-6

---
+++ chapter-6/script.js
@@ -0,0 +1,50 @@
+//    Express
+//
+//  
+//        ////frontend////          req     -> <-      res       ////backend////
+//                                        ask data
+//                              
+//                                                        CRUD operations karte hai in backend
+//
+//     -> ** sirf 4 work hote hai crud jab req jati hai frontend se toh in backend
+//
+//      http methods:
+//                  GET method 
+//                  POST method -> frontend pe data hai -> so send karo in backend and save it in backend
+//                  PUT / PATCH method -> update ke liye kaam aata hai
+//                  DELETE method -> delete karta hai
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+//
