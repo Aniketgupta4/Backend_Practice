b61f385f3a7e58b2ef2fb24b1cbf6e4686d1eef8
Aniketgupta4 <aniketkumargupta302@gmail.com>
2025-09-11 00:51:25 +0530

    chapter-6

---
+++ chapter-6/Basics.js
@@ -0,0 +1,51 @@
+//   -------------------Express(http sever create kar deta hai self -> npm init -> npm i express)----------------------
+//
+//  
+//        ////frontend////          req     -> <-      res       ////backend////
+//                                        ask data
+//                              
+//                                                        CRUD operations karte hai in backend
+//
+//     -> ** sirf 4 work hote hai crud jab req jati hai frontend se toh in backend
+//
+//      http methods:
+//                  GET method 
+//                  POST method -> frontend pe data hai -> so send karo in backend and save it in backend
+//                  PUT / PATCH method -> update ke liye kaam aata hai
+//                  DELETE method -> delete karta hai
+// 
+// 
+//         -> PATCH / PUT -> both method of update 
+//                     -> patch -> ex: ki age name dob -> so kisi 1 ko bs update karna hai so use patch
+//                     -> put -> ex: ki age name dob -> sabko update karna hai so use put 
+// 
+// 
+// 
+// 
+//          ex: //apna-frontend//      <->       //deepseek server//
+//                              api use karray hai
+//    ->  algorithms work hori hongi in server -> so we have to buy api -> b/c paisa lagra hai deepseek ka bhi manage karne mai
+// 
+//     
+//     -> **** open source -> code koi bhi use kar shakta hai -> but run hmko karna padega so uska setup apan ko karna hoga so cost time toh laga na apna apan ko compiler and all setup karna hoga 
+// 
+// 
+// 
+// 
+// 
+// 
+//       ->  ////frontend////          req     -> <-      res       ////backend////
+//                                           ask data
+//                                                                 http:linkedin/aniket4
+//                                                                   
+//          -> ****  so pata kaise chalge karna kya hai so -> pata chalga kya karna hai by -> methods="get/post/put/patch/delete"
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+//
--- chapter-6/script.js
+++ chapter-6/script.js
@@ -1,50 +1,47 @@
-//    Express
-//
-//  
-//        ////frontend////          req     -> <-      res       ////backend////
-//                                        ask data
-//                              
-//                                                        CRUD operations karte hai in backend
-//
-//     -> ** sirf 4 work hote hai crud jab req jati hai frontend se toh in backend
-//
-//      http methods:
-//                  GET method 
-//                  POST method -> frontend pe data hai -> so send karo in backend and save it in backend
-//                  PUT / PATCH method -> update ke liye kaam aata hai
-//                  DELETE method -> delete karta hai
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-//
+// node script.js -> new change karne pe again start karo -> so nodemon use karo -> itself start server -> npm i -g nodemon
+
+const express = require("express")
+
+const app = express();
+
+// --------------------------------------------------
+
+// normal
+app.use((req,res)=>{
+  //  res.send("hello");
+  // res.send({"name":"aniket","age":"24"});
+  res.send(["a","b",12]);
+})
+
+
+//-------------------------------------------------------
+
+// routing
+
+// **** ye issue create karega "/" -> ki "/home" ya "/about" -> karengai so "/" wala hi ayega -> isko "/" remove kar diye so sab shai chalega -> this is due to ye alag se routing handle karta hai b/c sabse pehle ye "/" dekhe ga kisi mai bhi ex: in "/about" pehle "/" aya so "/" isme pehle ghush gya and show output -> and niche check hi ni karta hai 
+// **** solve it by isko sabse last mai likho
+// app.use("/",(req,res)=>{
+//   res.send("page");
+// })
+
+// app.use("/home",(req,res)=>{
+//   res.send("home page");
+// })
+
+// app.use("/about",(req,res)=>{
+//   res.send("about page");
+// })
+
+// working well -> last mai likho so start matching from starting
+// app.use("/",(req,res)=>{
+//   res.send("page");
+// })
+
+// ----------------------------------------------------
+
+
+
+app.listen(4000,()=>{
+    console.log("listening to port 4000");
+})
+
